d42b2f2be876721fa9bd955f2d2854f23e1459b5
kimo18 <64424003+kimo18@users.noreply.github.com>
2023-01-03 13:42:09 +0200

    update by dev 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,10 @@ public class Main {
         System.out.print(Constant.Data_Type);
         System.out.print("you are the best");
     }
-
+    public void welcom()
+    {
+        System.out.print("HI");
+    }
 
     public static void main(String[] args)
     {

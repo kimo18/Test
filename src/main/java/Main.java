b5afb1386e7973354b1cdfa05170632ed34fdf9e
kimo18 <kimoalaa18@yahoo.com>
2023-01-03 10:46:44 +0200

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+
+    public void appData(String data)
+    {
+        System.out.print("Data "+ data);
+    }
+
+
+    public static void main(String[] args)
+    {
+        System.out.print("Hello");
+    }
+}
